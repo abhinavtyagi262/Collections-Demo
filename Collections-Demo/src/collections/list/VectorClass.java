0e96172dc27665650a244dbcfe495897ccf5e37c
Abhinav Tyagi <61839001+abhinavtyagi262@users.noreply.github.com>
2020-07-21 00:21:52 +0530

    Update VectorClass.java

--- Collections-Demo/src/collections/list/VectorClass.java
+++ Collections-Demo/src/collections/list/VectorClass.java
@@ -7,20 +7,20 @@ public class VectorClass {
 
 	public static void main(String[] args) {
 		
-		List<String> countries = new Vector<String>();		//insertion order is maintained and it is synchronized
+		List<String> countries = new Vector<String>();	//insertion order is maintained and it is synchronized
 		countries.add("India");
 		countries.add("USA");
 		countries.add("China");
 		countries.add("Germany");
 		countries.add("India");
 		
-		System.out.println(countries);
+		System.out.println(countries);			//printing object of list
 		
-		for(String country : countries) {			//iteration using for-each loop
+		for(String country : countries) {		//iteration using for-each loop
 			System.out.println(country);
 		}
 		
-		for(int i=0; i<countries.length(); i++){		//iteration using for loop
+		for(int i=0; i<countries.length(); i++){	//iteration using for loop
 			System.out.println(countries.get(i));
 		}	
 	}
