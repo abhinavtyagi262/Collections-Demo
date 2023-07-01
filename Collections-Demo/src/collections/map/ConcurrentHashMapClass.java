d657f6231a89e0a48a6a98acb522a31247eda0b6
Abhinav Tyagi <61839001+abhinavtyagi262@users.noreply.github.com>
2023-07-01 17:20:42 +0530

    Update ConcurrentHashMapClass.java

--- Collections-Demo/src/collections/map/ConcurrentHashMapClass.java
+++ Collections-Demo/src/collections/map/ConcurrentHashMapClass.java
@@ -8,7 +8,7 @@ public class ConcurrentHashMapClass {
 	public static void main(String[] args) {
 		
 		Map<Integer, String> numbers = new ConcurrentHashMap<Integer, String>();	//key-value pairs are stored and it is unsorted
-		numbers.put(1, "India");							//Concurrent operations are allowed
+		numbers.put(1, "India");							//concurrent operations are allowed
 		numbers.put(2, "USA");
 		numbers.put(4, "China");
 		numbers.put(5, "Germany");
